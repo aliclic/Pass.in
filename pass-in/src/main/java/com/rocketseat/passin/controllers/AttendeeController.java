60106d4ffd0837282eaa52e28e9f08baf5ccc4ff
alic <alicvictor11@gmail.com>
2024-04-04 20:07:24 -0300

    aula 3

--- pass-in/src/main/java/com/rocketseat/passin/controllers/AttendeeController.java
+++ pass-in/src/main/java/com/rocketseat/passin/controllers/AttendeeController.java
@@ -1,15 +1,32 @@
 package com.rocketseat.passin.controllers;
 
+import com.rocketseat.passin.dto.attendee.AttendeeBadgeResponseDTO;
+import com.rocketseat.passin.services.AttendeeService;
+import com.rocketseat.passin.services.CheckInService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/attendees")
+@RequiredArgsConstructor
 public class AttendeeController {
-    @GetMapping
-    public ResponseEntity<String> getTeste() {
-        return ResponseEntity.ok("sucesso!");
+    private final AttendeeService attendeeService;
+
+    @GetMapping("/{id}/badge")
+    public ResponseEntity<AttendeeBadgeResponseDTO> getAttendeeBadge(@PathVariable String id, UriComponentsBuilder uriComponentsBuilder) {
+        AttendeeBadgeResponseDTO response = this.attendeeService.getAttendeeBadge(id, uriComponentsBuilder);
+        return ResponseEntity.ok(response);
     }
+
+    @PostMapping("/{attendeeId}/check-in")
+    public ResponseEntity registerCheckIn(@PathVariable String attendeeId, UriComponentsBuilder uriComponentsBuilder) {
+        this.attendeeService.checkInAttendee(attendeeId);
+
+        var uri = uriComponentsBuilder.path("/attendees/{attendeeId}/badge").buildAndExpand(attendeeId).toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
+
 }
